acbe0bb2d86e13258f88febb3977a91e909e5bae
Tharun <105356462+dev-tharun@users.noreply.github.com>
2024-05-28 02:01:01 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from .validators import validate_custom_email,validate_mobile_number
 
 class Record(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	first_name = models.CharField(max_length=50)
 	last_name =  models.CharField(max_length=50)
-	email =  models.CharField(max_length=100)
-	phone = models.CharField(max_length=15)
+	email =  models.CharField(max_length=100,unique=True, validators=[validate_custom_email])
+	phone =  models.CharField(max_length=10, validators=[validate_mobile_number])
 	address =  models.CharField(max_length=100)
 	city =  models.CharField(max_length=50)
 	state =  models.CharField(max_length=50)
